92294a5173f461412615bba1cdbf52ba84f0e7c4
ajdez <anthony.desormeau@gmail.com>
2017-02-09 13:23:19 -0500

    put in loading and some css

--- app.js
+++ app.js
@@ -11,53 +11,57 @@
 // .then(response=>response.json())
 // .then(data => console.log(data));
 
-var DARKSKY_API_URL = 'https://api.darksky.net/forecast/';
-var DARKSKY_API_KEY = '2c409eb3da49a3706de9fe983f4b9781';
-var CORS_PROXY = 'https://cors-anywhere.herokuapp.com/';
 
-var GOOGLE_MAPS_API_KEY = 'AIzaSyDPjCuKDl0jj--DT7jzc8cafgRIxLpKb1I';
-var GOOGLE_MAPS_API_URL = 'https://maps.googleapis.com/maps/api/geocode/json';
+(function(){
+  var DARKSKY_API_URL = 'https://api.darksky.net/forecast/';
+  var DARKSKY_API_KEY = '2c409eb3da49a3706de9fe983f4b9781';
+  var CORS_PROXY = 'https://cors-anywhere.herokuapp.com/';
+
+  var GOOGLE_MAPS_API_KEY = 'AIzaSyDPjCuKDl0jj--DT7jzc8cafgRIxLpKb1I';
+  var GOOGLE_MAPS_API_URL = 'https://maps.googleapis.com/maps/api/geocode/json';
 
 
-function getCoordinatesForCity(cityName) {
-    var url = `${GOOGLE_MAPS_API_URL}?address=${cityName}&key=${GOOGLE_MAPS_API_KEY}`;
+  function getCoordinatesForCity(cityName) {
+      var url = `${GOOGLE_MAPS_API_URL}?address=${cityName}&key=${GOOGLE_MAPS_API_KEY}`;
+
+      return (
+          fetch(url)
+          .then(response => response.json())
+          .then(data => data.results[0].geometry.location)
+      );
+  }
+
+
+  function getCurrentWeather (coords){
+    var url = `${CORS_PROXY}${DARKSKY_API_URL}${DARKSKY_API_KEY}/${coords.lat},${coords.lng}?units=si&exclude=minutely,hourly,daily,alerts,flags`;
 
     return (
-        fetch(url)
-        .then(response => response.json())
-        .then(data => data.results[0].geometry.location)
-    );
-}
+      fetch(url)
+      .then(response => response.json())
+      .then(data => data.currently)
+    )
+  }
+
+  //////////////////////////// functions on top/////////////////////////
+
+  var app = document.querySelector("#app");
+  var cityForm = document.querySelector(".city-form");
+  var cityInput = document.querySelector(".city-input");
+  var cityWeather = document.querySelector(".city-weather");
 
 
-function getCurrentWeather (coords){
-  var url = `${CORS_PROXY}${DARKSKY_API_URL}${DARKSKY_API_KEY}/${coords.lat},${coords.lng}?units=si&exclude=minutely,hourly,daily,alerts,flags`;
-
-  return (
-    fetch(url)
-    .then(response => response.json())
-    .then(data => data.currently)
-  )
-}
-
-//////////////////////////// functions on top/////////////////////////
-
-var app = document.querySelector("#app");
-var cityForm = document.querySelector(".city-form");
-var cityInput = document.querySelector(".city-input");
-var weatherButton = document.querySelector(".get-weather-button");
-var cityWeather = document.querySelector(".city-weather");
+  cityForm.addEventListener('submit', function(e){
+    cityWeather.innerText = "Loading . . .";
+    var city = cityInput.value;
+    e.preventDefault();
 
 
-cityForm.addEventListener('submit', function(e){
-  var city = cityInput.value;
-  e.preventDefault();
-
-
-  getCoordinatesForCity(city)
-  .then(getCurrentWeather)
-  .then(function(weather){
-    console.log("EEEERRRFF")
-    cityWeather.innerText = 'the current weather is : ' + weather.temperature;
-  })
-});
+    getCoordinatesForCity(city)
+    .then(getCurrentWeather)
+    .then(function(weather){
+      console.log("EEEERRRFF")
+      cityWeather.innerText = "";
+      cityWeather.innerText = 'the current weather is : ' + weather.temperature;
+    })
+  });
+})();
